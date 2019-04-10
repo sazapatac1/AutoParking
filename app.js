31b709b39af5e5a0e49532f9eba95e67c6916e2c
sazapatac1 <saz4560@hotmail.com>
2019-04-10 14:07:35 -0500

    subiendo rama

--- app.js
+++ app.js
@@ -4,11 +4,14 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+const path = require('path');
+
 
 //api
 const api = require('./routes')
 
 //definiendo bodyparser
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 app.use('/api',api)
