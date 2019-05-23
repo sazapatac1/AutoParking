23a0edf913eba6ea47535858fd831066a2a5001a
csuazag <camilo.suaza12@gmail.com>
2019-05-23 00:46:43 -0500

    Script que corre en el pc del administrador.

--- Raspberry/MF/ReadAdminPC.py
+++ Raspberry/MF/ReadAdminPC.py
@@ -3,25 +3,7 @@ import serial
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-
-def testingPage(driver):
-    if driver.title == 'Actualizar Conductores':
-        return True
-    return False
-
-
-def writeInput(code):
-    x = ser.readline()
-    print(x.decode("utf-8"))
-    element = driver.find_element_by_id('id_internalCarnet')
-    element.clear()
-    element.send_keys(code)
-
-
-def main():
-    driver = webdriver.Chrome()
-    driver.get('http://autoparkingeafit.herokuapp.com/login')
-    ser = serial.Serial(
+ser = serial.Serial(
         port='COM3',
         baudrate = 9600,
         parity=serial.PARITY_NONE,
@@ -29,4 +11,30 @@ def main():
         bytesize=serial.EIGHTBITS,
         timeout=1)
 
-main()
+driver = webdriver.Chrome()
+driver.get('http://autoparkingeafit.herokuapp.com/login')
+
+
+def testingPage():
+    global driver
+    if driver.title == 'Actualizar Conductores':
+        return True
+    return False
+
+
+def writeInput(data):
+    element = driver.find_element_by_id('id_internalCarnet')
+    element.clear()
+    element.send_keys(data)
+    time.sleep(3)
+
+
+def listener():
+    while True:
+        x = ser.readline()
+        data = x.decode("utf-8")
+
+        if len(data) != 0 and testingPage():
+            writeInput(data)
+
+listener()
