20bb304663fa99a72274c56f0fe70d2cfbe1e203
csuazag <camilo.suaza12@gmail.com>
2019-05-26 11:56:12 -0500

    Cambios a nueva biblioteca

--- Lector-RFID/examples/ReadAdminRasp.py
+++ Lector-RFID/examples/ReadAdminRasp.py
@@ -1,33 +1,16 @@
 #!/usr/bin/env python
-# -*- coding: utf8 -*-
-#
-#    Copyright 2014,2018 Mario Gomez <mario.gomez@teubi.co>
-#
-#    This file is part of MFRC522-Python
-#    MFRC522-Python is a simple Python implementation for
-#    the MFRC522 NFC Card Reader for the Raspberry Pi.
-#
-#    MFRC522-Python is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Lesser General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    MFRC522-Python is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Lesser General Public License for more details.
-#
-#    You should have received a copy of the GNU Lesser General Public License
-#    along with MFRC522-Python.  If not, see <http://www.gnu.org/licenses/>.
-#
 
-import RPi.GPIO as GPIO
-import MFRC522
 import signal
-import requests
 import time
+import sys
 import serial
 
+from pirc522 import RFID
+
+run = True
+rdr = RFID()
+util = rdr.util()
+util.debug = True
 
 ser = serial.Serial(
         port=' /dev/ttyS0',
@@ -38,48 +21,29 @@ ser = serial.Serial(
         timeout=1
 )
 
-
-GPIO.setmode(GPIO.BOARD)
-
-
-continue_reading = True
-
-# Capture SIGINT for cleanup when the script is aborted
 def end_read(signal,frame):
-    global continue_reading
-    print "Ctrl+C captured, ending read."
-    continue_reading = False
-    GPIO.cleanup()
+    global run
+    print("\nCtrl+C captured, ending read.")
+    run = False
+    rdr.cleanup()
+    sys.exit()
 
-# Hook the SIGINT
 signal.signal(signal.SIGINT, end_read)
 
-# Create an object of the class MFRC522
-MIFAREReader = MFRC522.MFRC522()
+print("Starting")
+while run:
+    rdr.wait_for_tag()
 
-# Welcome message
-print "Press Ctrl-C to stop."
+    (error, data) = rdr.request()
+    if not error:
+        print("\nDetected: " + format(data, "02x"))
 
-# This loop keeps checking for chips. If one is near it will get the UID and authenticate
-while continue_reading:
-    
-    # Scan for cards    
-    (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+    (error, uid) = rdr.anticoll()
+    if not error:
+        print("Card read UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3]))
 
-    # If a card is found
-    if status == MIFAREReader.MI_OK:
-        print "Card detected"
-    
-    # Get the UID of the card
-    (status,uid) = MIFAREReader.MFRC522_Anticoll()
-
-    # If we have the UID, continue
-    if status == MIFAREReader.MI_OK:
-
-        # Print UID
-        print "Card read UID: %s,%s,%s,%s" % (uid[0], uid[1], uid[2], uid[3])
-        id_carne = str(uid[0]) + str(uid[1])  + str(uid[2]) + str(uid[3])
-        ser.write(id_carne)
+        id_carnet = str(uid[0])+str(uid[1])+str(uid[2])+str(uid[3])
+        ser.write(id_carnet)
         time.sleep(2)
         
         
--- Lector-RFID/examples/ReadParkingRasp.py
+++ Lector-RFID/examples/ReadParkingRasp.py
@@ -1,94 +1,55 @@
 #!/usr/bin/env python
-# -*- coding: utf8 -*-
-#
-#    Copyright 2014,2018 Mario Gomez <mario.gomez@teubi.co>
-#
-#    This file is part of MFRC522-Python
-#    MFRC522-Python is a simple Python implementation for
-#    the MFRC522 NFC Card Reader for the Raspberry Pi.
-#
-#    MFRC522-Python is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Lesser General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    MFRC522-Python is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Lesser General Public License for more details.
-#
-#    You should have received a copy of the GNU Lesser General Public License
-#    along with MFRC522-Python.  If not, see <http://www.gnu.org/licenses/>.
-#
 
-import RPi.GPIO as GPIO
-import MFRC522
 import signal
-import requests
 import time
+import sys
+import serial
+import requests
+
+from pirc522 import RFID
+
+run = True
+rdr = RFID()
+util = rdr.util()
+util.debug = True
+
 
 GPIO.setmode(GPIO.BOARD)
-
 GPIO.setup(32, GPIO.OUT) #Verde
 GPIO.setup(37, GPIO.OUT) #Rojo
 
-continue_reading = True
-
-# Capture SIGINT for cleanup when the script is aborted
 def end_read(signal,frame):
-    global continue_reading
-    print "Ctrl+C captured, ending read."
-    continue_reading = False
-    GPIO.cleanup()
+    global run
+    print("\nCtrl+C captured, ending read.")
+    run = False
+    rdr.cleanup()
+    sys.exit()
 
-# Hook the SIGINT
 signal.signal(signal.SIGINT, end_read)
 
-# Create an object of the class MFRC522
-MIFAREReader = MFRC522.MFRC522()
+print("Starting")
+while run:
+    rdr.wait_for_tag()
 
-# Welcome message
-print "Press Ctrl-C to stop."
+    (error, data) = rdr.request()
+    if not error:
+        print("\nDetected: " + format(data, "02x"))
 
-# This loop keeps checking for chips. If one is near it will get the UID and authenticate
-while continue_reading:
-    
-    # Scan for cards    
-    (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+    (error, uid) = rdr.anticoll()
+    if not error:
+        print("Card read UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3]))
 
-    # If a card is found
-    if status == MIFAREReader.MI_OK:
-        print "Card detected"
-    
-    # Get the UID of the card
-    (status,uid) = MIFAREReader.MFRC522_Anticoll()
-
-    # If we have the UID, continue
-    if status == MIFAREReader.MI_OK:
-
-        # Print UID
-        print "Card read UID: %s,%s,%s,%s" % (uid[0], uid[1], uid[2], uid[3])
-
-        id_carne = str(uid[0]) + str(uid[1])  + str(uid[2]) + str(uid[3])
-
-        print(id_carne)
+        id_carnet = str(uid[0])+str(uid[1])+str(uid[2])+str(uid[3])
         
-        r = requests.post('https://autoparkingeafit.herokuapp.com/api/verifyCarnet' ,data = {'id_carnetFind': id_carne})
-
-        r = r.json()
-
+        r = requests.post('https://autoparkingeafit.herokuapp.com/api/verifyCarnet' ,data = {'id_carnetFind': id_carnet}).json()
         
-
         if r['access']:
-            print ("Hay Acceso!!")
+            print ("You can enter!!")
             GPIO.output(32, GPIO.HIGH)
             time.sleep(5)
             GPIO.output(32, GPIO.LOW)
         else:
-            print("No Hay Acceso...")
+            print("You can't enter")
             GPIO.output(37, GPIO.HIGH)
             time.sleep(5)
             GPIO.output(37, GPIO.LOW)
-
-        
-        
